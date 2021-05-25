125e58a929b6a5a28ce6cdadbe6eae2fa34888aa
Paula <paulavimf@gmail.com>
2021-05-25 19:41:25 +0100

    finally solved error when running function when the api is no ready
    and the multiple handle declaration problem using one time flag and the api.exchange.api_data_fully_ready(state): in the right way

--- run_energyplus.py
+++ run_energyplus.py
@@ -3,7 +3,7 @@
 import sys
 sys.path.insert(0, '/usr/local/EnergyPlus-9-4-0')
 from pyenergyplus.api import EnergyPlusAPI
-from dummy_transformation import  set_people
+from dummy_transformation import  set_people, simple_decision_people
 
 one_time = True
 solar_sensor = 0
@@ -22,9 +22,46 @@ output = '/home/paula/Documentos/Doctorado/Desarrollo/EPProject/APItesting/'
 def my_handler(state):
     global one_time, solar_sensor, people_actuator, n, m, people_sensor, people, Radiation_Rate
     sys.stdout.flush()
-    if one_time:
-        # TODO debería pedir el handle solo una vez
-        if api.exchange.api_data_fully_ready(state):
+    if api.exchange.api_data_fully_ready(state):
+        if one_time:
+            solar_sensor = api.exchange.get_variable_handle(
+                state, u"Site Direct Solar Radiation Rate per Area", u"ENVIRONMENT"
+            )
+            people_actuator = api.exchange.get_actuator_handle(
+                state ,"People","Number of People","WEST ZONE PEOPLE"
+            )
+            people_sensor = api.exchange.get_variable_handle(
+                state,"Zone People Occupant Count", "West Zone" )
+        one_time = False
+        hour = api.exchange.hour(state)
+        day = api.exchange.day_of_month(state)
+        month = api.exchange.month(state)
+        Radiation_Rate  = api.exchange.get_variable_value(state, solar_sensor)
+
+        if hour == 8:
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            print(f"{people} average before actuator")
+        if hour == 9:
+            api.exchange.set_actuator_value(state,people_actuator,50)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"{people} average after actuator")
+        if hour == 22:
+            api.exchange.reset_actuator(state,people_actuator)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"{people} average after reset actuator")
+        if Radiation_Rate > 700:
+            api.exchange.set_actuator_value(state,people_actuator,set_people())
+            print(f"demasiado {Radiation_Rate}")
+
+def my_handler_function_decision(state):
+    global one_time, solar_sensor, people_actuator, n, m, people_sensor, people, Radiation_Rate
+    sys.stdout.flush()
+    # TODO debería pedir el handle solo una vez ??? no entiendo xq
+    if api.exchange.api_data_fully_ready(state):
+        if one_time:
             solar_sensor = api.exchange.get_variable_handle(
                 state, u"Site Direct Solar Radiation Rate per Area", u"ENVIRONMENT"
             )
@@ -35,31 +72,26 @@ def my_handler(state):
                 state,"Zone People Occupant Count", "West Zone" )
             if solar_sensor == -1 or people_actuator == -1:
                 sys.exit(1)
-        # one_time = False
-    hour = api.exchange.hour(state)
-    day = api.exchange.day_of_month(state)
-    month = api.exchange.month(state)
-    Radiation_Rate  = api.exchange.get_variable_value(state, solar_sensor)
-
-    if hour == 8:
-        people = api.exchange.get_variable_value(state,people_sensor)
-        print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
-        print(f"{people} average before actuator")
-    if hour == 9:
-        api.exchange.set_actuator_value(state,people_actuator,50)
-        print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
-        people = api.exchange.get_variable_value(state,people_sensor)
-        print(f"{people} average after actuator")
-    if hour == 22:
-        api.exchange.reset_actuator(state,people_actuator)
-        print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
-        people = api.exchange.get_variable_value(state,people_sensor)
-        print(f"{people} average after reset actuator")
-    if Radiation_Rate > 700:
-        api.exchange.set_actuator_value(state,people_actuator,set_people())
-        print(f"demasiado {Radiation_Rate}")
-
-
+        one_time = False
+        hour = api.exchange.hour(state)
+        day = api.exchange.day_of_month(state)
+        month = api.exchange.month(state)
+        Radiation_Rate  = api.exchange.get_variable_value(state, solar_sensor)
+        if hour == 8:
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            print(f"{people} average before actuator")
+        if hour == 9:
+            value = simple_decision_people(Radiation_Rate)
+            api.exchange.set_actuator_value(state,people_actuator,value)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"{people} average after actuator")
+        if hour == 22:
+            api.exchange.reset_actuator(state,people_actuator)
+            print(f"Radiation rate in w/m2 at {day} de {month} a las {hour}  is {Radiation_Rate}")
+            people = api.exchange.get_variable_value(state,people_sensor)
+            print(f"{people} average after reset actuator")
 
 
 
@@ -69,6 +101,7 @@ if __name__ == '__main__':
     print("this is called only once")
     # new_temp = set_new_temp()
     api.runtime.callback_begin_system_timestep_before_predictor(state, my_handler)
+    # api.runtime.callback_begin_system_timestep_before_predictor(state, my_handler_function_decision)
     # api.exchange.request_variable(state, "SITE OUTDOOR AIR DRYBULB TEMPERATURE", "ENVIRONMENT")
     # api.exchange.request_variable(state, "SITE OUTDOOR AIR DEWPOINT TEMPERATURE", "ENVIRONMENT")
     # api.exchange.request_variable(state, "Site Outdoor Air Humidity Ratio","ENVIRONMENT" )
